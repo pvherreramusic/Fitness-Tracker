ff83f2b3322fe546cf78bd818b38029240911764
Caleb <calebjhenderson@gmail.com>
2020-05-26 22:21:56 -0700

    made express server listen

--- db/index.js
+++ db/index.js
@@ -1,3 +1,10 @@
-const chalk = require(chalk);
-const { Client } = require(pg);
-const express = require(express); 
+const chalk = require('chalk');
+const { Client } = require('pg');
+
+const express = require('express'); 
+const PORT  = process.env.PORT || 3000;
+const app = express();
+app.listen( PORT, () => {
+    console.log(chalk.blue(`Server is listening on PORT:${ PORT }`));
+
+});
