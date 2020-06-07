147459960c0521fa7546ea2dcefc5be03a80c960
Caleb <calebjhenderson@gmail.com>
2020-06-06 22:13:27 -0700

    cleaned up client

--- db/client.js
+++ db/client.js
@@ -1,8 +1,3 @@
-
-const express = require('express'); 
-const PORT  = process.env.PORT || 3000;
-const app = express();
-const chalk = require('chalk');
 const { Client } = require('pg');
 const connectionString = 'postgres://localhost:5432/fitness-dev';
 const client = new Client(connectionString)
